daafcf75442354aa1ebe825c82e29bb5ddf0b142
Jonathan Tuzman <tuzmusic@gmail.com>
2018-12-22 22:51:09 +0100

    write songs#create

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -2,7 +2,7 @@ class SongsController < ApplicationController
   before_action :find_instance, only: [:show, :edit, :update, :destroy]
 
   def index
-    
+    @songs = Song.all
   end
 
   def show
@@ -14,7 +14,13 @@ class SongsController < ApplicationController
   end
 
   def create
-    
+    @song = Song.new(song_params)
+    if @song.valid? 
+      @song.save
+      redirect_to song_path(@song)
+    else
+      redirect_to new_song_path
+    end
   end
 
   def edit
@@ -22,15 +28,22 @@ class SongsController < ApplicationController
   end
 
   def update
-    
+    redirect_to song_path(@song)
   end
 
   def destroy
     
   end
 
+  private
+
+  def song_params
+    params.require(:song).permit(:title, :released, :release_year, :artist_name, :genre)
+  end
+
   def find_instance
     @song = Song.find(params[:id])
   end
+
 
 end
