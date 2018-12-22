3e3a5693f7402405f6c604eae7306a878fcaf862
Jonathan Tuzman <tuzmusic@gmail.com>
2018-12-22 16:51:14 +0100

    validate song, except for one song per artist per year

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,2 +1,13 @@
 class Song < ActiveRecord::Base
+  validates :title, presence: true
+  validates :released, inclusion: {in: [true, false]}
+  validates :release_year, presence: true, if: :released?,
+   numericality: { less_than_or_equal_to: Date.today.year}
+  validates :artist_name, presence: true
+  validate :one_song_per_artist_per_year
+  
+  def one_song_per_artist_per_year
+    artist_songs = Song.where(artist_name == self.artist_name)
+    binding.pry
+  end
 end
