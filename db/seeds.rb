2329c685e521e73c7d4f46b4416640138b06d9af
Jonathan Tuzman <tuzmusic@gmail.com>
2018-12-23 20:37:47 +0100

    done; waiting for later lessons for some requirements

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+Song.delete_all
 Song.create(title: "I Got a Girl", released: true, release_year: 1994, artist_name: "Tripping Daisy", genre: "Indie Rock")
 Song.create(title: "Field Day Jitters", released: true, release_year: 1998, artist_name: "Tripping Daisy", genre: "Indie Rock")
 Song.create(title: "Here Comes the Sun", released: true, release_year: 1969, artist_name: "The Beatles", genre: "Classic Rock")
