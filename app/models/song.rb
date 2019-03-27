1cb216f9f944d0802bd756445329c2958f9dfa8f
github username <github email address>
2019-03-26 20:42:13 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -5,5 +5,8 @@ class Song < ActiveRecord::Base
    validates :title, uniqueness: { scope: [:artist_name, :release_year] }
    validates :artist_name, presence: true
    validates :released, inclusion: { in: [true, false] }
+   validates :release_year, presence: true, if: :released?
+   validates :release_year, numericality: { less_than_or_equal_to: Time.current.year }, presence: true, if: :released
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   resources :songs
+
+
+root to: 'songs#index'
 end
