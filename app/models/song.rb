43d57838fab40976ec2f3b3b40a281f8fe51a89e
Diana Badas <diana.badas@gmail.com>
2019-05-29 17:14:47 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,15 +1,8 @@
-class Song < ApplicationRecord
-  validates :title, presence: true
-  validates :title, uniqueness: {
-    scope: %i[release_year artist_name],
-    message: "should happen once per year"
-  }
-  validates :released, inclusion: { in: [true, false] }
+class Song < ActiveRecord::Base
+
+  validates :title, presence: true, uniqueness: true
+  validates_presence_of :release_year, :if => :released?
+  validates :release_year, numericality: { less_than_or_equal_to: Date.today.year }, :if => :released?
   validates :artist_name, presence: true
-  with_options if: :released == true do |song|
-    song.validates :release_year, numericality: {
-      only_integer: true,
-      less_than_or_equal_to: ->(_brewery) {Date.current.year}
-      }
-  end
+
 end
