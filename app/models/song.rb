27b85b777ce9599af652e17b579868c40450dcbb
Jared Grove <jaredkgrove@gmail.com>
2019-08-06 17:01:37 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,25 +1,25 @@
 class Song < ApplicationRecord
-    validates :name, presence: true
+    validates :title, presence: true
     validate :unique_name_for_artist_name_and_release_year
     validates :released, inclusion: { in: [true, false] }
     validate :valid_release_year_if_released
     validates :artist_name, presence: true
 
-    def unique_name_for_artist_and_year
-        song = Song.find_by(self.name)
+    def unique_name_for_artist_name_and_release_year
+        song = Song.find_by(title: self.title)
         if !!song && song.artist_name == self.artist_name && song.release_year == self.release_year
-            errors.add(:name, "A song with this name has already been released by this artist this year")
+            errors.add(:title, "A song with this title has already been released by this artist this year")
         end
     end
 
     def valid_release_year_if_released
-        if self.released && !self.release_year 
+        if self.released 
+            if !self.release_year 
                 errors.add(:release_year, "A released song must have a release year")
+            elsif self.release_year > Time.new.year
+                errors.add(:title, "Release year must be less than or equal to the current year")
             end
-        end
-        if self.release_year > Time.new.year)
-            errors.add(:name, "Release year must be less than or equal to the current year")
-        end
 
+        end
     end
 end
