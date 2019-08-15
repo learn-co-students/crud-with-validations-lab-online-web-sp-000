c9b93ab496e8be0abd22253365de8d1c3bf489cb
Ramon Chavez <rachavez1974@gmail.com>
2019-08-15 16:56:25 -0400

    Finish basic listing, viewing and editing

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,24 @@
 class SongsController < ApplicationController
+  before_action :set_song, only: [:show, :edit, :update]
+  
+  def show
+    
+  end
+
+  def index
+    @songs = Song.all
+  end
+
+  def edit
+    
+  end
+
+  private
+    def song_params(*args)
+      params.require(:artist).permit(*args)
+    end
+
+    def set_song
+      @song = Song.find(params[:id])
+    end  
 end
