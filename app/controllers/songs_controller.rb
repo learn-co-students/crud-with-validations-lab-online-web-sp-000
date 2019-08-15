97d45e1eb3ebc330d7b8cef26ebe34bb4e1b13a7
Ramon Chavez <rachavez1974@gmail.com>
2019-08-15 17:11:58 -0400

    Add create

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,8 +1,20 @@
 class SongsController < ApplicationController
   before_action :set_song, only: [:show, :edit, :update]
   
-  def show
-    
+  def new
+    @song = Song.new
+  end
+
+  def create
+    @song = Song.new(song_params(params[:song].keys))
+      if @song.save
+        redirect_to @song
+      else
+        render 'new'
+      end
+  end
+
+  def show 
   end
 
   def index
@@ -10,12 +22,11 @@ class SongsController < ApplicationController
   end
 
   def edit
-    
   end
 
   private
     def song_params(*args)
-      params.require(:artist).permit(*args)
+      params.require(:song).permit(*args)
     end
 
     def set_song
