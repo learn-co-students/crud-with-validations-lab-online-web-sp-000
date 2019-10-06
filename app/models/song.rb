5d87c6805c2e3a2c325e889f41222d5fd2cff9c9
Allen <allenfp@gmail.com>
2019-10-06 14:47:04 -0700

    Done.

---
+++ app/models/song.rb
@@ -0,0 +1,9 @@
+class Song < ApplicationRecord
+    validates :title, presence: true
+    validates :title, uniqueness: {
+        scope: [:release_year, :artist_name]
+    }
+    validates :released, inclusion: { in: [true, false]}
+    validates :artist_name, presence: true
+
+end
