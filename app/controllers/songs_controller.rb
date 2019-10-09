3691f795d5e2fc3aadc4502027eb9d5884f1b8f1
jgrantps <jgrantps@hotmail.com>
2019-10-09 16:32:11 -0300

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -28,7 +28,11 @@ class SongsController < ApplicationController
 
     def update
         @song.update(song_params)
-        redirect_to song_path(@song)
+        if @song.valid?
+            redirect_to song_path(@song)
+        else
+            render :edit
+        end
     end
     
     def destroy
