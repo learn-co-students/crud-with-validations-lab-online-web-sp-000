62516a943004001bdebb690ae55411d10be1e005
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-11-19 19:47:43 -0600

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,18 +1,9 @@
 class Song < ApplicationRecord
-    validates :title, presence: true
-  validates :title, uniqueness: {
-    scope: %i[release_year artist_name],
-    message: 'cannot be repeated by the same artist in the same year'
-  }
-  validates :released, inclusion: { in: [true, false] }
-  validates :artist_name, presence: true
-
-  with_options if: :released? do |song|
-    song.validates :release_year, presence: true
-    song.validates :release_year, numericality: {
-      less_than_or_equal_to: Date.today.year
-    }
-  end
+        validates :title, presence: true, uniqueness: true
+        validates_presence_of :release_year, :if => :released?
+        validates :release_year, numericality: { less_than_or_equal_to: Date.today.year }, :if => :released?
+        validates :artist_name, presence: true
+      
 
     def released?
       released
