f3d6d45d35ae756e2a5069bb0f5d2453d1165547
rohanphillips <rohan@solutions-insurance.com>
2019-11-26 11:46:46 -0600

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,32 +1,53 @@
 require 'pry'
 class SongsController < ApplicationController
+  before_action :set_song!, only: [:show, :edit, :update]
+
   def index
     @songs = Song.all
   end
 
   def show
-    @song = Song.find(params[:id])
   end
 
   def edit
-    @song = Song.find(params[:id])
+  end
+
+  def new
+    @song = Song.new
   end
 
   def create
-    @song = Song.new(song_params(params))
-    binding.pry
-    @song.save
-    # if @song.valid?
-    #     @song.save
-    #     redirect_to song_path(@song)
-    # else
-    #   render :new
-    # end
+    @song = Song.new(song_params)
+    if @song.valid?
+        @song.save
+        redirect_to song_path(@song)
+    else
+      render :new
+    end
+  end
+
+  def update
+    @song.update(song_params)
+    if @song.valid?
+      @song.save
+      redirect_to song_path(@song)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    Song.destroy(params[:id])  
+    redirect_to action: "index"
   end
 
   private
 
-  def song_params(*args)
-    params.permit(*args)
+  def song_params
+    params.require(:song).permit(:title, :released, :release_year, :artist_name, :genre)
+  end
+
+  def set_song!
+    @song = Song.find(params[:id])
   end
 end
