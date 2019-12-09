6eb68797bd29ee451852acab3128aa76d485e54b
juan siu <juan.siu@gmail.com>
2019-12-09 04:10:14 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,2 +1,14 @@
 class Song < ApplicationRecord
+  validates :title, presence: true
+  validates :title, uniqueness: true
+  validates :released, presence: true
+  validates :release_year, presence: true, if: :released?
+  validates :release_year, numericality: { only_integer: true, less_than_or_equal_to: Date.today.year }
+  validates :artist_name, presence: true
+  validates :genre, presence: true
+
+  def released?
+    self.released == true 
+  end
+  
 end
