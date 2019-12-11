cf5186ec24cb63659e2871a2282eda36642cc3df
juan siu <juan.siu@gmail.com>
2019-12-11 04:23:19 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,9 +3,9 @@ class Song < ApplicationRecord
   validates :title, uniqueness: {scope: :release_year, message: "should happen once per year"}
   validates :released, inclusion: { in: [true, false] }
   validates :release_year, presence: true, if: :released?
-  validates :release_year, numericality: { only_integer: true, less_than_or_equal_to: Date.today.year }
-  #validates :release_year,  absence: true, if: :not_released?
+  validates :release_year, numericality: { only_integer: true, less_than_or_equal_to: Date.today.year }, allow_blank: true
   validates :artist_name, presence: true
+
 
   def released?
     self.released == true
