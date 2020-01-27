8b7c4b4b6a33c8a4b9588e352c5f3e8e5f210b6a
Chinedu Arinze <c.s.arinze@gmail.com>
2020-01-27 18:20:57 -0500

    Add all functionality to application. All test are passing

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,2 +1,11 @@
 class Song < ApplicationRecord
+    validates :title, presence: true
+    validates :title, uniqueness: { scope: [:release_year, :artist_name]}
+    validates :released, presence: true, allow_blank: true
+    validates :release_year, numericality: { less_than_or_equal_to: DateTime.now.year }, unless: :released?
+    validates :artist_name, presence: true
+
+    def released?
+        released != true
+    end
 end
