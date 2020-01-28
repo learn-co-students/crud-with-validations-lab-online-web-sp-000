d5835b249e6282e5debcf6cecb75fc87f617929d
zdsmit <zdsmith001@gmail.com>
2020-01-28 08:52:57 -0800

    Done.

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,29 @@
+class SongsController < ApplicationController
+
+  def index
+    @songs = Song.all
+  end
+
+  def show
+    @song = Song.find(params[:id])
+  end
+
+  def new
+    @song = Song.new
+  end
+
+  def create
+  end
+
+  def edit
+    @song = Song.find(params[:id])
+  end
+
+  def update
+  end
+
+  def destroy
+    @song = Song.find(params[:id])
+  end
+
+end
