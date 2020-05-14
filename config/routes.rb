d18b0e445cdc9ce37e52537a0bac820f7c21b939
jkellyphilly <joel.kelly.philly@gmail.com>
2020-05-14 16:54:17 -0400

    SongsController specs passing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :songs, only: [:index, :new, :create, :edit, :show]
+  resources :songs, only: [:index, :new, :create, :edit, :show, :update, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
