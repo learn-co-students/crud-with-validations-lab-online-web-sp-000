c4497ce99ebf7df9febbe7ee22cc43031f3f77e0
Cole Rau <colevrau@gmail.com>
2020-06-03 00:44:56 -0700

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,7 @@
 class Song < ApplicationRecord
-    validates :title, presence: true 
+    validates :title, presence: true, uniqueness: true
     validates :released, inclusion: { in: [true, false] } 
-    # validates :release_year, 
+    validates :release_year, numericality: { less_than_or_equal_to: Time.now.year }, if: :released?
     validates :artist_name, presence: true
+
 end
