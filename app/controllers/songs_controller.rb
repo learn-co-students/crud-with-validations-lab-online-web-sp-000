7421d2970d638bad1a6f2eec1d637c4939f1553e
yumacorn <reachryanmui@gmail.com>
2020-06-22 00:26:20 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -7,4 +7,37 @@ class SongsController < ApplicationController
     def show
         @song = Song.find(params[:id])
     end
+
+    def edit
+        @song = Song.find(params[:id])
+    end
+
+    def create
+        @song = Song.new(song_params)
+
+        if @song.save
+            redirect_to song_path(@song)
+        else
+            render :new
+        end
+    end
+
+    def update
+        @song = Song.find(params[:id])
+        if @song.valid?
+            @song.update(song_params)
+            binding.pry
+            redirect_to song_path(@song)
+        else
+            binding.pry
+            render :edit
+        end
+    end
+
+    private
+    
+        def song_params
+            params.require(:song).permit(:title, :released, :artist_name)
+        end
+
 end
