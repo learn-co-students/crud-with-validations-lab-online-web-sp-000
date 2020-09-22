2563e572bd93425b199569d2dbe2a665fe4a97da
ismayton <ian.mayton@gmail.com>
2020-09-22 12:59:00 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,11 +1,15 @@
 class Song < ApplicationRecord
-    validates :title, presence: true
-    validates :released, presence: true
-    validates :release_year, presence: true, if: :released?
-
-
-    def released?
-        self.released == true 
-    end 
+    validates :title, presence: true, uniqueness: { scope: :release_year }
+    validates_inclusion_of :released, :in => [true, false]
+    validates :release_year, presence: true, if: :released?, numericality: { less_than_or_equal_to: Time.now.year,  only_integer: true }
     
+    def released?
+        self.released == true
+    end 
+
+    def early?
+        binding.pry
+        self.release_year < Time.now.year
+    end 
+
 end
