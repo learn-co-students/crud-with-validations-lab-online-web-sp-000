c07853392704cc6fd118f6ea286f8d0c8849b094
ArslanInfinite <ali.arslan319@gmail.com>
2020-09-23 22:03:49 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,15 +1,16 @@
 class Song < ActiveRecord::Base
 
-  def released?
-    released
+  validates :title, presence: true
+  validates :title, uniqueness: { scope: %i[release_year artist_name] }
+  validates :artist_name, presence: true
+  validates :released, inclusion: { in: [true, false] }
+    with_options if: :released? do |song|
+      song.validates :release_year, presence: true
+      song.validates :release_year, numericality: { less_than_or_equal_to: Date.today.year }
   end
 
-  validates :title, presence: true
-  validates :artist_name, presence: true
-  validates :released, inclusion: { in: %w(true false) }
-    with_options if: :released? do |released|
-      released.validates :release_year, presence: true
-      released.validates :release_year, numericality: { less_than: Time.now.year }
+  def released?
+    released
   end
 
 end
