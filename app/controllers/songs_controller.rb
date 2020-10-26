10e1b3602a175f23bcb99b76679ebf49d912d5c2
Chad Lengefeld <chadlengefeld@gmail.com>
2020-10-26 01:28:24 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -28,10 +28,14 @@ class SongsController < ApplicationController
 
 
   def update
-    @song = Song.find(params[:id])
-    @song.update(song_params)
-    @song.save
-    redirect_to song_path(@song)
+      @song = Song.find(params[:id])
+      @song.update(song_params)
+    if @song.valid?
+      @song.save
+      redirect_to song_path(@song)
+    else
+      render :edit
+    end
   end
 
   private
