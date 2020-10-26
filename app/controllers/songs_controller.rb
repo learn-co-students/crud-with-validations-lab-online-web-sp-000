85046a2f1801ea5bd5f46064f958dd221907116f
Chad Lengefeld <chadlengefeld@gmail.com>
2020-10-26 01:34:08 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -38,6 +38,12 @@ class SongsController < ApplicationController
     end
   end
 
+  def destroy
+    @song = Song.find(params[:id])
+    @song.destroy
+    redirect_to songs_url
+  end
+
   private
 
     def song_params
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,6 @@
 class Song < ApplicationRecord
   validates :title, presence: true, uniqueness: true
   validates :released, inclusion: { in: [true, false] }
+  validates :artist_name, presence: true
 
 end
