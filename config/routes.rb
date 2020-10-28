f424dca1c1e5abe3e1a295781423e361865d5d98
LostnAustin <chadlengefeld@gmail.com>
2020-10-28 01:03:01 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  
- get '/songs', to: 'songs#index'
+root 'songs#index'
+
+get '/songs', to: 'songs#index'
  get '/songs/new', to: 'songs#new', as: 'new_song'
  post '/songs', to: 'songs#create'
  get '/songs/:id', to: 'songs#show', as: 'song'
