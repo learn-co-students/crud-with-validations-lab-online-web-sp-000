0a0c08c3692334962918b5b1890581a6b592e6e3
sparsell <hello@sparsell.com>
2020-11-12 10:40:51 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,12 +1,23 @@
 class Song < ApplicationRecord
 
-    validates :title, presence: true
-    #Cannot be repeated by the same artist in the same year
-    validates :artist_name, presence: true
-    validates :released, inclusion: { in: [true, false] }
-    # released_year - optional - if released is false
-    # released_year - required if released is true
-    #validates :relaseed_year,  - must be == or < current year
     
+    validates :artist_name, presence: true
 
+    #Cannot be repeated by the same artist in the same year
+    validates :title, uniqueness: {scope: [:release_year, :artist_name], message: "can't be released twice by the same artist in the same year"}
+    validates :title, presence: true
+    
+    # released_year - required if released is true
+    # released_year - optional - if released is false
+    #validates :release_year,  - must be == or < current year
+    validates :released, inclusion: { in: [true, false] }
+    with_options if: :released? do |song|
+        song.validates :release_year, presence: true
+        song.validates :release_year, numericality: {
+          less_than_or_equal_to: Time.now.year
+        }
+    end
+   
 end
+
+ 
