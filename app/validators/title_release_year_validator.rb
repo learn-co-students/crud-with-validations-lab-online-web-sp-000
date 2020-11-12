a08db6904ae5f89bed84553f0c86f04f1690264c
Joseph Perez <hello@josephdanielperez.com>
2020-11-12 15:28:52 -0800

    Done.

---
+++ app/validators/title_release_year_validator.rb
@@ -0,0 +1,10 @@
+class TitleReleaseYearValidator < ActiveModel::Validator
+
+    def validate(record)
+      song = Song.find_by(title: record.title)
+      if song && song.release_year == record.release_year
+        record.errors[:release_year] << "Can't release a song twice in the same year!"
+      end
+    end
+  
+end
