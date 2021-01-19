19eb9b88dbedef93dfd4eba95bae396b54060674
Amanda Rodgers <aarodgerswork@gmail.com>
2021-01-18 19:31:36 -0700

    runs resource generator for songs

--- db/migrate/20210119022847_create_songs.rb
+++ db/migrate/20210119022847_create_songs.rb
@@ -2,10 +2,10 @@ class CreateSongs < ActiveRecord::Migration[5.0]
   def change
     create_table :songs do |t|
       t.string :title
-      t.boolean :released
       t.integer :release_year
-      t.string :artist_name
+      t.boolean :released
       t.string :genre
+      t.string :artist_name
 
       t.timestamps
     end
