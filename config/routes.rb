9fd71a8157e5044ec4ee35f8c40607be6cd1232a
Amanda Rodgers <aarodgerswork@gmail.com>
2021-01-22 09:01:20 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   resources :songs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/songs', to: 'songs#index'
-  get '/songs/new', to: 'songs#new', as: 'new_song'
+  get '/songs/new', to: 'songs#new'
   post '/songs', to: 'songs#create'
-  get '/songs/:id', to: 'songs#show', as: 'song'
-  get '/songs/:id/edit', to: 'songs#edit', as: 'edit_song'
+  get '/songs/:id', to: 'songs#show'
+  get '/songs/:id/edit', to: 'songs#edit'
   patch '/songs/:id', to: 'songs#update'
   delete '/songs/:id', to: 'songs#destroy'
 
