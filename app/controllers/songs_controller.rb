9d3b787cc100697464043ace35917aafd20b83de
nllew <n.lewis.perc@gmail.com>
2021-01-23 15:02:18 -0600

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,6 +4,20 @@ class SongsController < ApplicationController
         @song = Song.new
     end
 
+    def create
+        @song = Song.new(song_params)
+
+        if @song.save
+            redirect_to song_path(@song)
+        else
+            render :new
+        end
+    end
+
+    def index
+        @songs = Song.all
+    end
+
     def show
         @song = Song.find(params[:id])
     end
@@ -12,7 +26,24 @@ class SongsController < ApplicationController
         @song = Song.find(params[:id])
     end
 
+    def update
+        @song = Song.find(params[:id])
+
+        if @song.update(song_params)
+            redirect_to song_path(@song)
+        else
+            render :edit
+        end
+    end    
+
     def destroy
-        Song.sing(params[:id])
+        Song.find(params[:id]).destroy
+        redirect_to songs_url
+    end
+
+    private
+
+    def song_params
+        params.require(:song).permit(:title, :release_year, :released, :genre, :artist_name)
     end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -3,9 +3,15 @@ class Song < ApplicationRecord
     validates :title, presence: true
     validates :title, uniqueness: { scope: [:artist_name, :release_year] }
     validates :released, inclusion: [true, false]
-    validates_presence_of :release_year, :unless => :released?
-    # validates :release_year, 
-    # validates :release_year,
     validates :artist_name, presence: true
+
+    with_options if: :released? do |song|
+        song.validates_presence_of :release_year
+        song.validates :release_year, numericality: { less_than_or_equal_to: Date.today.year }
+    end
+
+    def released?
+        released
+    end
 
 end
