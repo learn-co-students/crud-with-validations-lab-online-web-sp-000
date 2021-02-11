a0bab12d5bd68ef121876b13810e8823877e6219
amcmullen9291 <amcmullen9291@gmail.com>
2021-02-10 23:19:11 -0500

    Done.

---
+++ db/seeds.rb
@@ -0,0 +1,2 @@
+@song = Song.create!(title: "good song", released: "true", artist_name: "Prince", release_year: "1999")
+@song2 = Song.create!(title: "better song", released: "false", artist_name: "Fleetwood Mac")
