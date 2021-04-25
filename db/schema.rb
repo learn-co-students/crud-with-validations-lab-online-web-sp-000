f739a218529bd89ab60e2156608d72478160bc82
Jess Pesale <jess_pesale@aol.com>
2021-04-25 17:43:27 -0400

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20210425201931) do
+
+  create_table "songs", force: :cascade do |t|
+    t.boolean  "released"
+    t.string   "artist_name"
+    t.string   "genre"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.string   "title"
+    t.integer  "release_year"
+  end
 
 end
