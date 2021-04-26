a0dd2ee3eb7f63a5c2786b9cc4fddca654cefdb9
Christerle Nounoute <christerlesn@gmail.com>
2021-04-26 03:39:33 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,6 @@
 class Song < ApplicationRecord
   validates :title, presence: true
+  validates :title, uniqueness: {scope: [:release_year, :artist_name]}
   validates :artist_name, presence: true
   validates :released, inclusion: [true, false]
   validates :release_year, presence: true, if: :released_is_true?
