5fff933be5467a8a13156216c7adcde7ea09dd82
Christerle Nounoute <christerlesn@gmail.com>
2021-04-26 02:30:55 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,6 @@
 class Song < ApplicationRecord
   validates :title, presence: true
   validates :artist_name, presence: true
+  validates :released, inclusion: [true, false]
+  validates :released, exclusion: [nil]
 end
