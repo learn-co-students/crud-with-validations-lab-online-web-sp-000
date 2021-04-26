3718c8f8788a88f67712230be4e3febf0526a994
Christerle Nounoute <christerlesn@gmail.com>
2021-04-26 01:47:14 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,36 @@
 class SongsController < ApplicationController
+  def new
+    @song = Song.new(song_params)
+  end
+
+  def create
+    @song = Song.new(song_params)
+    if @song.valid?
+      @song.save
+      redirect_to song_path(@song)
+    else
+      render :new
+    end
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def song_params
+    params.require(:song).permit(:title, :released, :release_year, :artist_name, :genre)
+  end
 end
